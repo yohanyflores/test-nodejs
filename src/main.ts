762ff71de4ae805a90ebdd77d76a3a47d7c3ac5c
Yohany Flores <yohanyflores@gmail.com>
2021-10-18 00:41:50 +0000

    Authenticatin en el API/DOC

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Catch, ArgumentsHost } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+
+// Para mostrar los errores en el log, que con el metodo anterior no lo montaba.
+@Catch()
+export class AllExceptionsFilter extends BaseExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    super.catch(exception, host);
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new AllExceptionFilter());
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.useGlobalInterceptors(new TimeoutInterceptor());
   app.useGlobalPipes(new ValidationPipe());
 
@@ -15,6 +26,16 @@ async function bootstrap() {
     .setTitle('Walter Backend')
     .setDescription('Walter App')
     .setVersion('1.0.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', description: 'Autentication con tokens' },
+      'bearerAuth',
+    )
+    .addSecurity('bearerAuth', {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      description: 'Autentication con tokens',
+    })
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
